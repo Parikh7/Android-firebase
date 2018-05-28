9c8103bf7da13afdbe75763a6a000e2bde85a8a7
Parikh oberoi <parikhoberoi330@gmail.com>
2018-05-28 20:56:54 +0530

    Add files via upload

---
+++ generated/source/buildConfig/debug/com/example/parikh/atry/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.parikh.atry;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.parikh.atry";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
